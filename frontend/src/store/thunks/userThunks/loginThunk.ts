512bb75c0606a8706e8d3c40cc3197d79386a82f
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-04-08 14:59:03 +0300

    Add login request

---
+++ frontend/src/store/thunks/userThunks/loginThunk.ts
@@ -0,0 +1,17 @@
+// import { LoginAPI } from '../../../api/LoginAPI'
+// import { LoginActions, LoginParamsType } from '../../reducers/LoginReducer'
+//
+// export const loginThunk =
+//   (data: LoginParamsType) =>
+//   (
+//     dispatch: any,
+//     // getStore: GetAppStoreType
+//   ) => {
+//     dispatch(LoginActions.setLoading(true))
+//     console.log(data)
+//     LoginAPI.signIn(data).then(res => {
+//       if (res.data.resultCode === 0) {
+//         console.log('jjj')
+//       }
+//     })
+//   }
